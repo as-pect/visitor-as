efe448dad5b64890163366897d998db9baca5973
Willem Wyndham <willem@cs.umd.edu>
2020-07-07 16:30:18 -0400

    Combine decorator tests into single file

--- __tests__/decorators.spec.ts
+++ __tests__/decorators.spec.ts
@@ -32,3 +32,20 @@ describe("List", () => {
     ]);
   });
 });
+
+const HelloWorldYay: string = `
+@capitalize
+const hello = \`hello\`;
+@capitalize
+const world = "world";
+@capitalize
+const yay = 'yay';
+`;
+
+describe("Capitilize", () => {
+  it("should handle simple struct", () => {
+    expect(
+      compileExample(HelloWorldYay, "./src/examples/capitalize.ts")
+    ).toStrictEqual(["hello -> HELLO", "world -> WORLD", "yay -> YAY"]);
+  });
+});
