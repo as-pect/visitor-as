50ec2f6193e58b25ff3e037d3b8b51ee96af2098
Willem Wyndham <willem@cs.umd.edu>
2020-07-08 16:22:55 -0400

    Move tests to same file

--- __tests__/tests.spec.ts
+++ __tests__/tests.spec.ts
@@ -1,4 +1,5 @@
 import { compileExample } from "./setup";
+import { SimpleParser, ASTBuilder } from '../dist';
 
 const FOO: string = `
 @list
@@ -48,4 +49,35 @@ describe("Capitilize", () => {
       compileExample(HelloWorldYay, "./src/examples/capitalize.ts")
     ).toStrictEqual(["hello -> HELLO", "world -> WORLD", "yay -> YAY"]);
   });
+});
+
+
+function expr(s: string): void {
+  expect(ASTBuilder.build(SimpleParser.parseExpression(s)))
+    .toBe(s)
+}
+
+function stmt(s: string): void {
+  expect(ASTBuilder.build(SimpleParser.parseStatement(s)))
+    .toBe(s)
+}
+
+describe("Parser", () => {
+  describe("Expressions", () => {
+    it("binary", ()=>{
+      expr("1 + 1");
+    });
+
+    it("call", () => {
+      expr("callFunction()");
+    });
+  });
+
+  describe("Statements", () => {
+    it("assignment", ()=>{
+      stmt("let x = 1");
+    });
+
+  });
+
 });
