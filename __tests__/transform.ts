5112dfb9759f6049ba9ebaf3445b973e3e0ba9b1
Willem Wyndham <willem@cs.umd.edu>
2021-04-01 16:53:19 -0400

    fix: add visit method in capitalize example

--- __tests__/transform.ts
+++
@@ -1,8 +0,0 @@
-import { compileAndRun } from "./setup";
-
-
-describe('hello world transform', () => {
-  it("should not throw", () => {
-    compileAndRun("assert(foo() == 'hello world', 'should equal')", "./src/examples/functionCallTransform.ts")
-  });
-});
--- src/examples/capitalize.ts
+++ src/examples/capitalize.ts
@@ -1,6 +1,6 @@
 import { cloneNode } from "../utils";
 import { VariableDecorator, registerDecorator } from "../decorator";
-import { StringLiteralExpression, VariableDeclaration } from "../../as";
+import { StringLiteralExpression, TemplateLiteralExpression, VariableDeclaration } from "../../as";
 
 class CapitalizeVisitor extends VariableDecorator {
   visitVariableDeclaration(node: VariableDeclaration): void {
@@ -16,6 +16,15 @@ class CapitalizeVisitor extends VariableDecorator {
     newNode.value = node.value.toUpperCase();
     this.replaceCurrentNode(newNode);
     this.stdout.write(node.value + " -> " + newNode.value + "\n");
+  }
+  
+  visitTemplateLiteralExpression(node: TemplateLiteralExpression): void {
+    if (node.parts.length == 1 && node.expressions.length == 0){
+      const newNode = cloneNode(node);
+      newNode.parts[0] = node.parts[0].toUpperCase();
+      this.replaceCurrentNode(newNode);
+      this.stdout.write(node.parts[0] + " -> " + newNode.parts[0] + "\n");
+    }
   }
 }
 
