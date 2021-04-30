b2798159426135f7538051e25129795d81e9fe0e
yjhmelody <465402634@qq.com>
2021-04-30 15:20:02 +0800

    dist: update and fix transformer.d.ts

--- dist/transformer.d.ts
+++ dist/transformer.d.ts
@@ -5,14 +5,14 @@ import { PathVisitor } from "./path";
 import { BaseTransformVisitor } from "./baseTransform";
 declare class Transform extends _Transform {
 }
-declare const ASTTransformVisitor_base: import("ts-mixer/dist/types").Class<[], BaseVisitor & Transform, {
+declare const ASTTransformVisitor_base: import("ts-mixer/dist/types/types").Class<[], BaseVisitor & Transform, {
     prototype: BaseVisitor;
 } & {
     prototype: Transform;
 }>;
 export declare class ASTTransformVisitor extends ASTTransformVisitor_base {
 }
-declare const ASTBuilderVisitor_base: import("ts-mixer/dist/types").Class<[], ASTBuilder & Transform, {
+declare const ASTBuilderVisitor_base: import("ts-mixer/dist/types/types").Class<[], ASTBuilder & Transform, {
     prototype: ASTBuilder;
     build: typeof ASTBuilder.build;
 } & {
@@ -20,7 +20,7 @@ declare const ASTBuilderVisitor_base: import("ts-mixer/dist/types").Class<[], AS
 }>;
 export declare class ASTBuilderVisitor extends ASTBuilderVisitor_base {
 }
-declare const PathTransformVisitor_base: import("ts-mixer/dist/types").Class<[], PathVisitor & Transform, {
+declare const PathTransformVisitor_base: import("ts-mixer/dist/types/types").Class<[], PathVisitor & Transform, {
     prototype: PathVisitor;
 } & {
     prototype: Transform;
@@ -28,7 +28,7 @@ declare const PathTransformVisitor_base: import("ts-mixer/dist/types").Class<[],
 export declare class PathTransformVisitor extends PathTransformVisitor_base {
 }
 export declare function mergeTransformer(from: Transform, to: Transform): void;
-declare const TransformVisitor_base: import("ts-mixer/dist/types").Class<[], BaseTransformVisitor & Transform, {
+declare const TransformVisitor_base: import("ts-mixer/dist/types/types").Class<[], BaseTransformVisitor & Transform, {
     prototype: BaseTransformVisitor;
 } & {
     prototype: Transform;
